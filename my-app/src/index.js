243909109da981a46cf74c2983296b886b55ad23
MonzerIdris <monzeridris01@gmail.com>
2022-08-27 03:23:52 +0400

    zzzzzzz

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import {Provider} from "react-redux"
@@ -22,8 +22,8 @@ const queryClient = new QueryClient({
 });
 
  
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <Context>
@@ -31,7 +31,9 @@ root.render(
       </Context>
     <ReactQueryDevtools />
   </QueryClientProvider>
-</React.StrictMode>);
+</React.StrictMode>,
+document.getElementById("root")
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
