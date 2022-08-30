88c2f48c290e1d7c47c49ef2875bd70ed17116cd
MonzerIdris <monzeridris01@gmail.com>
2022-08-31 03:57:41 +0400

    axios

--- my-app/src/api/axios.js
+++ my-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let BASE_URL = window.location.origin;
 
-if (BASE_URL === "http://localhost:3000" ) BASE_URL = "http://localhost:5000/api"
+if (BASE_URL == "http://localhost:3000" ) BASE_URL = "http://localhost:5000/api"
 
 else if (BASE_URL === "https://eshopp-heroku.herokuapp.com" ) BASE_URL = "https://eshopp-heroku.herokuapp.com/api" 
 
