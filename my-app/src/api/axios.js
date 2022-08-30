8966ec83c60265a08e9dd1359809530155a0853c
MonzerIdris <monzeridris01@gmail.com>
2022-08-31 00:34:34 +0400

    xd

--- my-app/src/api/axios.js
+++ my-app/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 let BASE_URL = window.location.origin;
 
-if (BASE_URL == "http://localhost:3000" ) BASE_URL = "http://localhost:5000/api"
+// if (BASE_URL == "http://localhost:3000" ) BASE_URL = "http://localhost:5000/api"
 
 console.log(BASE_URL)
 export default axios.create({
